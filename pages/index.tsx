dd91c1c50e7f7a9b7155430edea47fc6155e66d4
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-04-17 00:53:38 +0530

    Added SEO tags.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from 'next/Head';
 import React from 'react';
 import Header from '../components/Header';
-import { Badge } from 'react-badges';
-import NotificationIcon from '../icons/NotificationIcon';
-import CodeSnippet from '../components/code-snippet/CodeSnippet';
-import { firstSnippet, secondSnippet, thirdSnippet, fourthSnippet, fifthSnippet } from '../snippet-data/Snippets';
 import IntroSection from '../components/sections/IntroSection';
 import BasicNotificationBadgeSection from '../components/sections/BasicNotificationBadgeSection';
 import MaxValueSection from '../components/sections/MaxValueSection';
@@ -13,10 +9,30 @@ import BadgeAlignmentSection from '../components/sections/BadgeAlignmentSection'
 import RegularBadges from '../components/sections/RegularBadges';
 
 export default function Home() {
+	const url = '';
+	const description =
+		'This is a React Component Library. With help of this you can create Customizable and Reusable React Badge component for your all kinds of Projects and WebSites.';
+	const image =
+		'https://res.cloudinary.com/dg2zkumuc/image/upload/v1618564550/React%20Badges/react-badges_oarucd.gif';
+	const title = 'React Badges';
 	return (
 		<React.Fragment>
 			<Head>
-				<title>React Badges</title>
+				<title>{title}</title>
+				<meta name="og:title" content={title} />
+				<meta name="og:type" content="article" />
+				<meta name="twitter:card" content="summary" />
+				<meta name="twitter:site" content="@publisher_handle" />
+				<meta name="twitter:title" content={title} />
+				<meta name="description" content={description} />
+				<meta name="og:description" content={description} />
+				<meta name="twitter:description" content={description} />
+				<meta name="image" content={image} />
+				<meta name="twitter:image" content={image} />
+				<meta name="og:image" content={image} />
+				<React.Fragment>
+					<meta name="og:url" content={url} />
+				</React.Fragment>
 			</Head>
 			<Header />
 			<IntroSection />
